146d7463987a7b199269e724c7aa59929ec8df43
torstein-vik <torsteinv64@gmail.com>
2018-10-23 20:22:55 +0200

    Added offset to lazylist

--- src/util/lazylist.sage
+++ src/util/lazylist.sage
@@ -4,15 +4,18 @@ import itertools
 import operator
 
 class LazyList:
-    def __init__(self, data = lambda n: 0, cache=True, printed=None):
+    def __init__(self, data = lambda n: 0, offset=0, cache=True, printed=None):
         self.data = data
         self.doCache = cache
+        self.offset = offset
         self.cache = {}
         self.printed = printed
         
     def __getitem__(self, n):
         if n in self.cache and self.doCache:
             return self.cache[n]
+        if n < self.offset:
+            raise IndexError("Index of LazyList too low! Given index was {} and the offset is {}".format(n, self.offset))
         data = self.data(n)
         if hasattr(data, 'simplify_full'):
             data = data.simplify_full()
@@ -21,15 +24,16 @@ class LazyList:
         return data
     
     def __eq__(self, other):
-        print ("It's impossible to calculate if two infinite sequences are equal... returning false")
-        return False
+        if self.offset != other.offset or self.toList(20) != other.toList(20):
+            return False
+        raise NotImplementedError("It's impossible to calculate if two infinite sequences are equal")
     
     def __iter__(self):
-        for n in (itertools.count() if printed==None else range(self.printed)):
+        for n in (itertools.count(self.offset) if self.printed==None else range(self.offset, self.offset + self.printed)):
             yield self[n]
     
     def toList(self, length):
-        return [self[i] for i in range(self.printed if length == None and self.printed != None else length)]
+        return [self[i] for i in range(self.offset, self.offset + (self.printed if length == None and self.printed != None else length))]
     
     def __add__(self, B):
         return liftBinaryOpToLazyList(operator.add)(self, B)
@@ -39,23 +43,29 @@ class LazyList:
     
     def __repr__(self, length = None):
         res = ""
-        for i in range((20 if self.printed == None else self.printed) if length == None else length):
+        for i in range(self.offset, self.offset + (20 if self.printed == None else self.printed) if length == None else length):
             res += str(self[i]) + ", "
         return res[:-2] + ", ..."
     
     def compress(self, k):
+        if self.offset != 0:
+            raise NotImplementedError("Can't compress list with non-zero offset")
         return LazyList(data = lambda n: self[n * k], printed = self.printed)
     
     def expand(self, k, filler = lambda n: 0):
+        if self.offset != 0:
+            raise NotImplementedError("Can't expand list with non-zero offset")
         return LazyList(data = lambda n: self[n / k] if n % k == 0 else filler(n), printed = self.printed)
     
     def applyRecursiveTransform(self, transformCoeffIndexTriples):
+        if self.offset != 0:
+            raise NotImplementedError("Can't apply recursive transform to list with non-zero offset")
         list = LazyList(cache = True, printed=self.printed)
         list.data = lambda n: sum([coeff * (1 if orIndex == "id" else self[orIndex]) * (1 if recIndex == "id" else list[recIndex]) for coeff, orIndex, recIndex in transformCoeffIndexTriples[n]])
         return list
     
     def fmap(self, f, cache = True):
-        return LazyList(data = lambda n: f(self[n]), cache=cache, printed=self.printed)
+        return LazyList(data = lambda n: f(self[n]), cache=cache, printed=self.printed, offset=self.offset)
 
     def toNearestGaussianIntegers(self):
         return self.fmap(toNearestGaussianInteger)
@@ -68,15 +78,17 @@ BellAntiTransform = LazyList(data = lambda n: [(1, "id", "id")] if n == 0 else [
 
 
 def liftBinaryOpToLazyList(op, exclude = -1):
-    return lambda X, Y: LazyList(data = lambda n: op(X[n], Y[n]) if n > exclude else X[n], cache=True, printed = min(X.printed, Y.printed))
+    return lambda X, Y: LazyList(data = lambda n: op(X[n], Y[n]) if n > exclude else X[n], cache=True, printed = min(X.printed, Y.printed), offset = max(X.offset, Y.offset))
 
 def liftUnaryOpToLazyList(func, exclude = -1):
-    return lambda X: LazyList(data = lambda n: func(X[n]) if n > exclude else X[n], cache=True, printed = X.printed)
+    return lambda X: LazyList(data = lambda n: func(X[n]) if n > exclude else X[n], cache=True, printed = X.printed, offset = X.offset)
 
 def toLazyList(list):
     return LazyList(data = lambda n: list[n] if n < len(list) else 0, cache = False, printed = len(list))
 
 def DirichletConvolution(X, Y):
+    if X.offset != 0 or Y.offset != 0:
+        raise NotImplementedError("Can't convolute lists with non-zero offset")
     return LazyList(data = lambda n: sum(X[d] * Y[n - d] for d in range(n + 1)), cache = True, printed = min(X.printed, Y.printed))
 
 PointwiseProduct = liftBinaryOpToLazyList(operator.mul)
--- zetatypes/util/lazylist.py
+++ zetatypes/util/lazylist.py
@@ -8,15 +8,18 @@ import itertools
 import operator
 
 class LazyList:
-    def __init__(self, data = lambda n: _sage_const_0 , cache=True, printed=None):
+    def __init__(self, data = lambda n: _sage_const_0 , offset=_sage_const_0 , cache=True, printed=None):
         self.data = data
         self.doCache = cache
+        self.offset = offset
         self.cache = {}
         self.printed = printed
         
     def __getitem__(self, n):
         if n in self.cache and self.doCache:
             return self.cache[n]
+        if n < self.offset:
+            raise IndexError("Index of LazyList too low! Given index was {} and the offset is {}".format(n, self.offset))
         data = self.data(n)
         if hasattr(data, 'simplify_full'):
             data = data.simplify_full()
@@ -25,15 +28,16 @@ class LazyList:
         return data
     
     def __eq__(self, other):
-        print ("It's impossible to calculate if two infinite sequences are equal... returning false")
-        return False
+        if self.offset != other.offset or self.toList(_sage_const_20 ) != other.toList(_sage_const_20 ):
+            return False
+        raise NotImplementedError("It's impossible to calculate if two infinite sequences are equal")
     
     def __iter__(self):
-        for n in (itertools.count() if printed==None else range(self.printed)):
+        for n in (itertools.count(self.offset) if self.printed==None else range(self.offset, self.offset + self.printed)):
             yield self[n]
     
     def toList(self, length):
-        return [self[i] for i in range(self.printed if length == None and self.printed != None else length)]
+        return [self[i] for i in range(self.offset, self.offset + (self.printed if length == None and self.printed != None else length))]
     
     def __add__(self, B):
         return liftBinaryOpToLazyList(operator.add)(self, B)
@@ -43,23 +47,29 @@ class LazyList:
     
     def __repr__(self, length = None):
         res = ""
-        for i in range((_sage_const_20  if self.printed == None else self.printed) if length == None else length):
+        for i in range(self.offset, self.offset + (_sage_const_20  if self.printed == None else self.printed) if length == None else length):
             res += str(self[i]) + ", "
         return res[:-_sage_const_2 ] + ", ..."
     
     def compress(self, k):
+        if self.offset != _sage_const_0 :
+            raise NotImplementedError("Can't compress list with non-zero offset")
         return LazyList(data = lambda n: self[n * k], printed = self.printed)
     
     def expand(self, k, filler = lambda n: _sage_const_0 ):
+        if self.offset != _sage_const_0 :
+            raise NotImplementedError("Can't expand list with non-zero offset")
         return LazyList(data = lambda n: self[n / k] if n % k == _sage_const_0  else filler(n), printed = self.printed)
     
     def applyRecursiveTransform(self, transformCoeffIndexTriples):
+        if self.offset != _sage_const_0 :
+            raise NotImplementedError("Can't apply recursive transform to list with non-zero offset")
         list = LazyList(cache = True, printed=self.printed)
         list.data = lambda n: sum([coeff * (_sage_const_1  if orIndex == "id" else self[orIndex]) * (_sage_const_1  if recIndex == "id" else list[recIndex]) for coeff, orIndex, recIndex in transformCoeffIndexTriples[n]])
         return list
     
     def fmap(self, f, cache = True):
-        return LazyList(data = lambda n: f(self[n]), cache=cache, printed=self.printed)
+        return LazyList(data = lambda n: f(self[n]), cache=cache, printed=self.printed, offset=self.offset)
 
     def toNearestGaussianIntegers(self):
         return self.fmap(toNearestGaussianInteger)
@@ -72,15 +82,17 @@ BellAntiTransform = LazyList(data = lambda n: [(_sage_const_1 , "id", "id")] if 
 
 
 def liftBinaryOpToLazyList(op, exclude = -_sage_const_1 ):
-    return lambda X, Y: LazyList(data = lambda n: op(X[n], Y[n]) if n > exclude else X[n], cache=True, printed = min(X.printed, Y.printed))
+    return lambda X, Y: LazyList(data = lambda n: op(X[n], Y[n]) if n > exclude else X[n], cache=True, printed = min(X.printed, Y.printed), offset = max(X.offset, Y.offset))
 
 def liftUnaryOpToLazyList(func, exclude = -_sage_const_1 ):
-    return lambda X: LazyList(data = lambda n: func(X[n]) if n > exclude else X[n], cache=True, printed = X.printed)
+    return lambda X: LazyList(data = lambda n: func(X[n]) if n > exclude else X[n], cache=True, printed = X.printed, offset = X.offset)
 
 def toLazyList(list):
     return LazyList(data = lambda n: list[n] if n < len(list) else _sage_const_0 , cache = False, printed = len(list))
 
 def DirichletConvolution(X, Y):
+    if X.offset != _sage_const_0  or Y.offset != _sage_const_0 :
+        raise NotImplementedError("Can't convolute lists with non-zero offset")
     return LazyList(data = lambda n: sum(X[d] * Y[n - d] for d in range(n + _sage_const_1 )), cache = True, printed = min(X.printed, Y.printed))
 
 PointwiseProduct = liftBinaryOpToLazyList(operator.mul)
